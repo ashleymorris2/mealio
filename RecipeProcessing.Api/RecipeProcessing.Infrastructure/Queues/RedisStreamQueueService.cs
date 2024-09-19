29cab7308f595943d777486f790b1427a54be4e4
Ash M <ashleym9000@gmail.com>
2024-09-19 01:27:01 +0100

    added max stream length

--- RecipeProcessing.Api/RecipeProcessing.Infrastructure/Queues/RedisStreamQueueService.cs
+++ RecipeProcessing.Api/RecipeProcessing.Infrastructure/Queues/RedisStreamQueueService.cs
@@ -5,11 +5,9 @@ namespace RecipeProcessing.Infrastructure.Queues;
 
 public class RedisStreamQueueService(IConnectionMultiplexer redis) : IQueueService
 {
-    private readonly IConnectionMultiplexer _redis = redis;
-
     public async Task EnqueueImageProcessingTaskAsync(Stream imageStream, string contentType, string imageHash)
     {
-        var database = _redis.GetDatabase();
+        var database = redis.GetDatabase();
         
         using var memoryStream = new MemoryStream();
         await imageStream.CopyToAsync(memoryStream);
@@ -19,6 +17,6 @@ public class RedisStreamQueueService(IConnectionMultiplexer redis) : IQueueServi
             new NameValueEntry(nameof(imageBase64), imageBase64),
             new NameValueEntry(nameof(contentType), contentType),
             new NameValueEntry(nameof(imageHash), imageHash)
-        ]);
+        ], maxLength: 500, useApproximateMaxLength: true  );
     }
 }
