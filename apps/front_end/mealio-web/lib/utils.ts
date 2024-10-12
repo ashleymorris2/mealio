9081a8d704a74d018da51a731f2d973f996343a0
Ash M <ashleym9000@gmail.com>
2024-10-13 00:52:18 +0100

    Added null check for time formatting string

--- apps/front_end/mealio-web/lib/utils.ts
+++ apps/front_end/mealio-web/lib/utils.ts
@@ -1,4 +1,10 @@
 export function formatTime(time: string): string {
+  if (!time || typeof time !== "string") {
+    throw new Error(
+      "Invalid input: Time must be a string in the format 'HH:MM'."
+    );
+  }
+
   const [hours, minutes] = time.split(":");
   const formattedHours = hours !== "00" ? `${hours}h ` : "";
   return `${formattedHours} ${minutes}m`;
